d0feb59864df21547157cd92c449dc358cb86ca0
Marcel Riera <mrier011@fiu.edu>
2019-04-28 23:47:18 -0400

    Added color transition to tree.js

--- js/tree/main.js
+++ js/tree/main.js
@@ -18,7 +18,7 @@ function recursiveDraw(pos, v){
   let magnitude = v.mag();
   if (magnitude < 5)
     return;
-  stroke(0, 120, 0, map(magnitude,5,100,20,150));
+  stroke(255-map(magnitude,5,100,0,255), 120, 0, map(magnitude,5,100,20,150));
   strokeWeight(map(magnitude,5,100,1,thickness));
   line(pos.x, pos.y, pos.x + v.x, pos.y + v.y);
   var nextPos = createVector(pos.x + v.x, pos.y + v.y);
